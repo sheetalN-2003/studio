019e2f71b28bfa7a09ff7438ae96179cee597086
Sheetal N <sheethaln927@gmail.com>
2025-09-24 16:09:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "genosym-ai",
+  "appId": "1:51969964134:web:06e288173b10fe20ab55a7",
+  "apiKey": "AIzaSyCoTgaBep-mEAOMNroDKqfzENUP8aYg2L8",
+  "authDomain": "genosym-ai.firebaseapp.com",
+  "measurementId": "G-74JTGQ93BX",
+  "messagingSenderId": "51969964134"
+};
